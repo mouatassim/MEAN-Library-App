465e7adf73dc6b20231d823522bee0aa49a5f81d
Mouatassim <motalei4991@gmail.com>
2018-10-04 11:51:48 +0100

    add model and api to get users

--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -1,8 +1,4 @@
-const mongoose = require('mongoose');
 const Users = require('../../models/Users');
-const UserSession = require('../../models/UserSession');
-const bcrypt = require('bcrypt');
-
 module.exports = function(app){
 
        app.get('/users', function(req, res){
